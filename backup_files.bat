e9508c4639bc2250bb87623ff4a1cc9a4cdc1a49
KentaGoto <gotokentag@gmail.com>
2021-10-11 08:51:44 +0900

    Delete comments, and minor correction

--- backup_files.bat
+++ backup_files.bat
@@ -1,16 +1,12 @@
 @echo off
 
 
-rem バックアップ元の起点ディレクトリ。
 SET SRC=D:
-rem バックアップ先の起点ディレクトリ。ネットワークドライブでもok。
-SET DST=<どこ起点に置きたいか>
+SET DST=E:bkup
 
 
-rem サブルーチンを呼んでバックアップ対象のフォルダを指定。複数指定できる。
 CALL :backup \tool
 
-rem ここでexitしないとバグるよ
 exit
 
 :backup
